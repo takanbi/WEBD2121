bfacd104f2475a1ca855a1da35f2491ccd59d0f9
takanbi <tunji.akanbi@century.edu>
2023-10-24 15:29:07 -0500

    minor tweaks

--- week7/js/script.js
+++ week7/js/script.js
@@ -1,5 +1,5 @@
-$(document).ready(function(){
-    $("#menu").click(function(){
-        $("#navigation").toggle();
-    });
-});
+// $(document).ready(function(){
+//     $("#menu").click(function(){
+//         $("#navigation").toggle();
+//     });
+// });
