f69b33bacdc9d0f3dd81db1095be08c8b27482a2
takanbi <tunji.akanbi@century.edu>
2023-10-26 13:22:08 -0500

    fixed responsive navigation issues

--- week7/js/script.js
+++ week7/js/script.js
@@ -1,5 +1,10 @@
-// $(document).ready(function(){
-//     $("#menu").click(function(){
-//         $("#navigation").toggle();
-//     });
-// });
+$(document).ready(function(){
+    let navLinks = document.querySelectorAll(".nav-links");
+    console.log(navLinks);
+    $("#menu").click(function() {
+        $("#navigation").toggle(animateLinks());
+    });
+    function animateLinks() {
+        gsap.from($("#navigation"), { y: -100, opacity:0, duration: .25})
+    }
+});
