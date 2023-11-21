ce6449b0c8b536f6454a2ee7d146e101f3c32b3d
takanbi <tunji.akanbi@century.edu>
2023-11-21 15:35:36 -0600

    added carousel

--- week12/js/index.js
+++ week12/js/index.js
@@ -1,1 +1,9 @@
 import "../node_modules/bootstrap/dist/js/bootstrap.bundle.min.js";
+
+const myCarouselElement = document.querySelector('#myCarousel')
+
+const carousel = new bootstrap.Carousel(myCarouselElement, {
+  interval: 2000,
+  touch: false,
+  pause: "hover"
+})
